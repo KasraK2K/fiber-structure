9ff531498b79ce1c631b842b63dcaae87b3aea8e
Kasra Karami <Kasra_K2K@yahoo.com>
2023-02-14 19:42:52 +0330

    first commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/csrf"
+	"github.com/gofiber/fiber/v2/middleware/etag"
+	"github.com/gofiber/fiber/v2/middleware/favicon"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
+	"log"
+	"time"
+)
+
+func main() {
+	app := fiber.New()
+
+	//Middlewares
+	app.Use(cache.New())
+	app.Use(compress.New())
+	app.Use(cors.New())
+	app.Use(csrf.New())
+	app.Use(etag.New())
+	app.Use(favicon.New())
+	app.Use(limiter.New(limiter.Config{Max: 100, Expiration: 60 * time.Second}))
+	app.Use(logger.New())
+	app.Use(recover.New())
+	app.Use(requestid.New())
+
+	//Configs
+	app.Static("/", "./public")
+
+	//Routes
+	app.Get("/metrics", monitor.New(monitor.Config{Title: "Default Metrics Page"}))
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World!")
+	})
+
+	//Init & Log
+	log.Fatal(app.Listen(":3000"))
+}
